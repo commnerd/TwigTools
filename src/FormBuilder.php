bbbf2f815c717df65459127106aee830cfd8462c
root <root@ip-172-31-6-204.us-west-2.compute.internal>
2018-08-03 20:31:32 +0000

    Update to work in newer and older environments

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -7,14 +7,39 @@ use Twig_Markup;
 
 class FormBuilder {
 
+    /**
+     * Twig reference
+     */
     private $_twig;
 
+    /**
+     * Load the library
+     */
     public function __construct(Twig_Environment $twig) {
         $this->_twig = $twig;
     }
 
+    /**
+     * Text area definition
+     */
     public function textarea(array $hash)
     {
-        return new Twig_Markup($this->_twig->render('FormBuilder/textarea.html', $hash), 'UTF-8');
+        return $this->_render('textarea.html', $hash);
+    }
+
+    /**
+     * Text input definition
+     */
+    public function textinput(array $hash)
+    {
+        return $this->_render('textinput.html', $hash);
+    }
+
+    /**
+     * Render logic for returning a Twig_Markup object (so escaping doesn't get executied)
+     */
+    private function _render($template, array $hash)
+    {
+        return new Twig_Markup($this->_twig->render("FormBuilder/$template", $hash), 'UTF-8');
     }
 }
